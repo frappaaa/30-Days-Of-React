2408f1c9c95a0c3152653449070de846be3f0605
Francesco Luppi <francescoluppi@aol.com>
2021-01-14 18:29:20 +0100

    Modify the standard app

--- 05_Day_Props/05_props_boilerplate/src/index.js
+++ 05_Day_Props/05_props_boilerplate/src/index.js
@@ -1,55 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import asabenehImage from './images/asabeneh.jpg'
-
-// Fuction to show month date year
-
-const showDate = (time) => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ]
-
-  const month = months[time.getMonth()].slice(0, 3)
-  const year = time.getFullYear()
-  const date = time.getDate()
-  return ` ${month} ${date}, ${year}`
-}
-
-// Header Component
-const Header = ({
-  data: {
-    welcome,
-    title,
-    subtitle,
-    author: { firstName, lastName },
-    date,
-  },
-}) => {
-  return (
-    <header>
-      <div className='header-wrapper'>
-        <h1>{welcome}</h1>
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
-        <p>
-          {firstName} {lastName}
-        </p>
-        <small>{showDate(date)}</small>
-      </div>
-    </header>
-  )
-}
 
 // TechList Component
 const TechList = ({ techs }) => {
@@ -68,84 +18,33 @@ const UserCard = ({ user: { firstName, lastName, image } }) => (
   </div>
 )
 
-// A button component
 
-const Button = ({ text, onClick, style }) => (
-  <button style={style} onClick={onClick}>
-    {text}
-  </button>
-)
-
-const buttonStyles = {
-  backgroundColor: '#61dbfb',
-  padding: 10,
-  border: 'none',
-  borderRadius: 5,
-  margin: 3,
-  cursor: 'pointer',
-  fontSize: 18,
-  color: 'white',
-}
 
 // Main Component
-const Main = ({ user, techs, greetPeople, handleTime }) => (
+const Main = (props) => (
   <main>
     <div className='main-wrapper'>
       <p>Prerequisite to get started react.js:</p>
       <ul>
-        <TechList techs={techs} />
+        <TechList techs={props.techs} />
       </ul>
-      <UserCard user={user} />
-      <Button text='Greet People' onClick={greetPeople} style={buttonStyles} />
-      <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
+      <UserCard user={props.user} />
     </div>
   </main>
 )
 
-// Footer Component
-const Footer = ({ copyRight }) => (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>Copyright {copyRight.getFullYear()}</p>
-    </div>
-  </footer>
-)
 
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
-  const data = {
-    welcome: 'Welcome to 30 Days Of React',
-    title: 'Getting Started React',
-    subtitle: 'JavaScript Library',
-    author: {
-      firstName: 'Asabeneh',
-      lastName: 'Yetayeh',
-    },
-    date: new Date(), // date needs to be formatted to a human readable format
-  }
-  const date = new Date()
+  const user = {name:'Francesco', surname:'Luppi'}
   const techs = ['HTML', 'CSS', 'JavaScript']
-  // copying the author from data object to user variable using spread operator
-  const user = { ...data.author, image: asabenehImage }
-
-  const handleTime = () => {
-    alert(showDate(new Date()))
-  }
-  const greetPeople = () => {
-    alert('Welcome to 30 Days Of React Challenge, 2020')
-  }
-
   return (
     <div className='app'>
-      <Header data={data} />
       <Main
         user={user}
         techs={techs}
-        handleTime={handleTime}
-        greetPeople={greetPeople}
       />
-      <Footer copyRight={date} />
     </div>
   )
 }
